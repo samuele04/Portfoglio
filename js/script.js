3cc2a1f21881e067f0a770e9eb2ce8d238602709
samuele04 <72665897+samuele04@users.noreply.github.com>
2021-10-31 17:44:10 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 
+function menu() {
+    var x = document.getElementById('nav');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+
+    x.classList.add("fixed");
+}
